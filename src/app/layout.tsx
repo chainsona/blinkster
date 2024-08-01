954b592e4c6bda39373d46bca84445e337f82a6b
Soona <code@chainsona.dev>
2024-08-01 15:08:10 +0200

    feat(metadata): update information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,48 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "iBlink",
+const DATA = {
+  name: "iBlink",
   description: "No-code Blinks builder for everyone",
+  url: "https://iblink.to",
+  images: ["https://iblink.to/iblinkto.gif"],
+};
+
+export const metadata: Metadata = {
+  metadataBase: new URL(DATA.url),
+  title: {
+    default: DATA.name,
+    template: `%s | ${DATA.name}`,
+  },
+  description: DATA.description,
+  openGraph: {
+    title: `${DATA.name}`,
+    description: DATA.description,
+    url: DATA.url,
+    siteName: `${DATA.name}`,
+    locale: "en_US",
+    type: "website",
+    images: [DATA.image],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: `${DATA.name}`,
+    card: "summary_large_image",
+  },
+  verification: {
+    google: "",
+    yandex: "",
+  },
 };
 
 export default function RootLayout({
